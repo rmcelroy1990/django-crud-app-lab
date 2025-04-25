a2499c8347cf5de064276ab9198649ae5dce126b
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-04-25 18:41:21 -0500

    implemented full CRUD for birds

--- birdcollector/my_app/views.py
+++ birdcollector/my_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Bird
 
 from django.http import HttpResponse
 def home(request):
@@ -8,19 +10,37 @@ def about(request):
     return render(request, 'about.html')
 
 def bird_index(request):
+    birds = Bird.objects.all()
     return render(request, 'birds/index.html', {'birds':birds})
 
-class Bird:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+def bird_detail(request, bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    return render(request, 'birds/detail.html', {'bird': bird})
+
+class BirdCreate(CreateView):
+    model = Bird
+    field = ['name', 'breed', 'description', 'age']
+
+class BirdUpdate(UpdateView):
+    model = Bird
+    fields = ['breed', 'description', 'age']
+
+class BirdDelete(DeleteView):
+    model = Bird
+    success_url = '/birds/'
 
 
-birds = [
-    Bird('Lolo', 'tabby', 'Kinda rude.', 3),
-    Bird('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Bird('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Bird('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# class Bird:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+
+# birds = [
+#     Bird('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Bird('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Bird('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Bird('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
